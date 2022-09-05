db9fccb74f1cc6bdf087ac145577bc9c97567ff5
borisavila539 <borisavila539@gmail.com>
2022-09-05 17:26:33 -0600

    tabla de pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre:string = 'Capitan America';
+  nombre2: string = 'boris albert avila lanza';
+  arreglo = [1,2,3,4,5,6,7,8,9];
+  PI:number = Math.PI;
+  Porcetaje:number = 0.234;
+  salario:number = 1234.5;
+  fecha:Date = new Date();
+  idioma : string = 'es';
+  password: string = 'holamundo';
+  activar :boolean = true;
+  videoURL:string = 'https://www.youtube.com/embed/mWRsgZuwf_8';
+
+  valorPromesa =  new Promise<string>((resolve)=>{
+    setTimeout(()=>{
+      resolve('Llego la data');
+    },4500);
+  });
+
+  heroe = {
+    nombre: 'logan',
+    clave: 'Wolverin',
+    edad: 500,
+    direccion:{
+      calle: 'Primera',
+      casa:20
+    }
+  }
 }
